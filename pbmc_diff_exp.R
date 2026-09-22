b8f31eff188daee21d54dba6cd947a1852331d7f
MuzhdahW <215532731+MuzhdahW@users.noreply.github.com>
2026-09-22 13:44:23 -0400

    added code to create a new column in the df that will allow me to compare active vs remission disease status within each seurate cluster

--- pbmc_diff_exp.R
+++ pbmc_diff_exp.R
@@ -7,9 +7,13 @@ library(Seurat)
 install.packages("patchwork")
 library(patchwork)
 install.packages("devtools")
+install.packages("dplyr")
+library(dplyr)
+install.packages("tibble")
+library(tibble)
 #devtools::install_github("immunogenomics/presto")
 #install.packages("presto")
-library(presto)
+#library(presto)
 #install.packages("GEOquery")
 #library(GEOquery)
 # Since the data being utilized is a pre-made RDS object, I am simply going to use CURL to open the file
@@ -38,5 +42,33 @@ crohns_subset <- subset(cleaned_pbmc, cells = pbmc_crohns_rows)
 pbmc_uc_rows <- rownames(cleaned_pbmc@meta.data)[grepl("UC", cleaned_pbmc@meta.data$barcode)]
 uc_subset <- subset(cleaned_pbmc, cells = pbmc_uc_rows)
 
+# now we see how many levels are in the orig.ident of each subset
+print(unique(crohns_subset@meta.data$seurat_clusters))
+print(unique(uc_subset@meta.data$seurat_clusters))
+# we have twelve cell clusters. These, however, are independent from "active" or "remission" disease.
+# in order to compare "active" or "remission" status within a cell cluster ( ie cluster 0 active vs remission or cluster 1 active vs remission)
+# I need to create a third column in the metadata table. I'm going to call this column "active disease"
+# and list it as "true" or "false". I will then combine that w the factor column, so I can compare
+# cluster 0 active vs cluster 0 remission, for example
 
+
+# just to play around for now I'm going to copy the dataframes. I am going to use dplyr to 
+# create the new columns. but I need to use rownames_to_column and column_to_rowname to preserve the 
+# barcodes, because the barcodes are the rownames from the metadata table. 
+
+copy_crohns <- crohns_subset
+copy_uc <- uc_subset
+
+# using dplyr to add in a column that has " active " or "remission" so I can compare active vs remission
+# within a given cluster more easily!We can call that column " status" and have it be " active " or "remission" 
+copy_uc@meta.data <- copy_uc@meta.data %>% 
+rownames_to_column(var = "cell_barcodes")%>% 
+mutate(status = if_else(grepl('Active',barcode),"active","remission")) %>%
+column_to_rownames(var = "cell_barcodes") 
+
+
+copy_crohns@meta.data <- copy_crohns@meta.data %>% 
+  rownames_to_column(var = "cell_barcodes")%>% 
+  mutate(status = if_else(grepl('Active',barcode),"active","remission")) %>%
+  column_to_rownames(var = "cell_barcodes")
 
